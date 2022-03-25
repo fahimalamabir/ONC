01d5a71ae5feb4f10f7b38cef625dbed7dcc4d89
Fahim Sahariar Alam <62411283+fahimalamabir@users.noreply.github.com>
2022-03-25 23:12:33 +0600

    Update main.py

--- DataAvailable/main.py
+++ DataAvailable/main.py
@@ -17,7 +17,7 @@ import sys
 import plotly.express as px
 
 st.cache(suppress_st_warning=True)
-st.set_page_config (layout='wide', page_title='daqt-apps', page_icon='ONCData.png')
+st.set_page_config (layout='wide', page_title='daqt-apps', page_icon='onc_dataTeam_logo.png')
 
 #Create a new environment with the latest version of python:
 
