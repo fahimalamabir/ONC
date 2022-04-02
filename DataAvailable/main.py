7d8035dc15afcacdada53a619fdc744303c8c783
Fahim Sahariar Alam <62411283+fahimalamabir@users.noreply.github.com>
2022-04-02 13:46:56 +0600

    Update main.py

--- DataAvailable/main.py
+++ DataAvailable/main.py
@@ -231,7 +231,7 @@ if pwd:
                                 )
                      )
             st.plotly_chart(fig)
-            index = open("dataVail.html").read().format(pwd1=pwd,eltT= elT,flT1 = flT,deviceD1 =deviceD,deviceZ1=deviceZ) 
+            index = open("DataAvailable/dataVail.html").read().format(pwd1=pwd,eltT= elT,flT1 = flT,deviceD1 =deviceD,deviceZ1=deviceZ) 
                                          
            
 
